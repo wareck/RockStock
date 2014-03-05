6251fe2f95914e0770799b22597f0d84c220cd8a
wareck <wareck@gmail.com>
2014-03-05 20:18:24 +0100

    first commit

---
+++ Marlin/Visual Micro/.Marlin.vsarduino.h
@@ -0,0 +1,94 @@
+#ifndef _VSARDUINO_H_
+#define _VSARDUINO_H_
+//Board = Arduino Mega or Mega 2560 w/ ATmega2560 (Mega 2560)
+#define __AVR_ATmega2560__
+#define ARDUINO 150
+#define ARDUINO_MAIN
+#define __AVR__
+#define __avr__
+#define F_CPU 16000000L
+#define __cplusplus
+#define __inline__
+#define __asm__(x)
+#define __extension__
+#define __ATTR_PURE__
+#define __ATTR_CONST__
+#define __inline__
+#define __asm__ 
+#define __volatile__
+
+#define __builtin_va_list
+#define __builtin_va_start
+#define __builtin_va_end
+#define __DOXYGEN__
+#define __attribute__(x)
+#define NOINLINE __attribute__((noinline))
+#define prog_void
+#define PGM_VOID_P int
+            
+typedef unsigned char byte;
+extern "C" void __cxa_pure_virtual() {;}
+
+
+#include "c:\arduino-1.5.6-r2\hardware\arduino\avr\cores\arduino\arduino.h"
+#include "c:\arduino-1.5.6-r2\hardware\arduino\avr\variants\mega\pins_arduino.h" 
+#include "C:\printer\Marlin\Marlin.pde"
+#include "C:\printer\Marlin\BlinkM.cpp"
+#include "C:\printer\Marlin\BlinkM.h"
+#include "C:\printer\Marlin\Configuration.h"
+#include "C:\printer\Marlin\ConfigurationStore.cpp"
+#include "C:\printer\Marlin\ConfigurationStore.h"
+#include "C:\printer\Marlin\Configuration_adv.h"
+#include "C:\printer\Marlin\DOGMbitmaps.h"
+#include "C:\printer\Marlin\LiquidCrystalRus.cpp"
+#include "C:\printer\Marlin\LiquidCrystalRus.h"
+#include "C:\printer\Marlin\Marlin.h"
+#include "C:\printer\Marlin\Marlin.ino"
+#include "C:\printer\Marlin\MarlinSerial.cpp"
+#include "C:\printer\Marlin\MarlinSerial.h"
+#include "C:\printer\Marlin\Marlin_main.cpp"
+#include "C:\printer\Marlin\Sd2Card.cpp"
+#include "C:\printer\Marlin\Sd2Card.h"
+#include "C:\printer\Marlin\Sd2PinMap.h"
+#include "C:\printer\Marlin\SdBaseFile.cpp"
+#include "C:\printer\Marlin\SdBaseFile.h"
+#include "C:\printer\Marlin\SdFatConfig.h"
+#include "C:\printer\Marlin\SdFatStructs.h"
+#include "C:\printer\Marlin\SdFatUtil.cpp"
+#include "C:\printer\Marlin\SdFatUtil.h"
+#include "C:\printer\Marlin\SdFile.cpp"
+#include "C:\printer\Marlin\SdFile.h"
+#include "C:\printer\Marlin\SdInfo.h"
+#include "C:\printer\Marlin\SdVolume.cpp"
+#include "C:\printer\Marlin\SdVolume.h"
+#include "C:\printer\Marlin\Servo.cpp"
+#include "C:\printer\Marlin\Servo.h"
+#include "C:\printer\Marlin\cardreader.cpp"
+#include "C:\printer\Marlin\cardreader.h"
+#include "C:\printer\Marlin\digipot_mcp4451.cpp"
+#include "C:\printer\Marlin\dogm_font_data_marlin.h"
+#include "C:\printer\Marlin\dogm_lcd_implementation.h"
+#include "C:\printer\Marlin\fastio.h"
+#include "C:\printer\Marlin\language.h"
+#include "C:\printer\Marlin\motion_control.cpp"
+#include "C:\printer\Marlin\motion_control.h"
+#include "C:\printer\Marlin\pins.h"
+#include "C:\printer\Marlin\planner.cpp"
+#include "C:\printer\Marlin\planner.h"
+#include "C:\printer\Marlin\qr_solve.cpp"
+#include "C:\printer\Marlin\qr_solve.h"
+#include "C:\printer\Marlin\speed_lookuptable.h"
+#include "C:\printer\Marlin\stepper.cpp"
+#include "C:\printer\Marlin\stepper.h"
+#include "C:\printer\Marlin\temperature.cpp"
+#include "C:\printer\Marlin\temperature.h"
+#include "C:\printer\Marlin\thermistortables.h"
+#include "C:\printer\Marlin\ultralcd.cpp"
+#include "C:\printer\Marlin\ultralcd.h"
+#include "C:\printer\Marlin\ultralcd_implementation_hitachi_HD44780.h"
+#include "C:\printer\Marlin\ultralcd_st7920_u8glib_rrd.h"
+#include "C:\printer\Marlin\vector_3.cpp"
+#include "C:\printer\Marlin\vector_3.h"
+#include "C:\printer\Marlin\watchdog.cpp"
+#include "C:\printer\Marlin\watchdog.h"
+#endif
